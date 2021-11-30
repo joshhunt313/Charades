89825872d72b5b8e96164634ed04bb9967c05c14
Josh Hunt <joshhruns@gmail.com>
2021-11-30 10:36:39 -0500

    Better words

--- charades.py
+++ charades.py
@@ -2,10 +2,12 @@ import requests
 import json
 import time
 
-NUM_ROUNDS = 10;
+NUM_ROUNDS = 10
+
+numPlayers = input("Numbers of players: ")
 
 for roundNum in range(10):
-    response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+    response = requests.get("https://random-word-form.herokuapp.com/random/noun")
     pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1")
     print(response.json()[0] + " " + str(pointResponse.json()[0]))
-    time.sleep(60)
+
