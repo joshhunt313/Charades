a82a284d458bdd6cd10efa8fefa9ee4733e120fd
Josh Hunt <joshhruns@gmail.com>
2021-11-30 10:41:16 -0500

    Added turns

--- charades.py
+++ charades.py
@@ -2,12 +2,16 @@ import requests
 import json
 import time
 
-NUM_ROUNDS = 10
+NUM_WORDS = 10
 
 numPlayers = input("Numbers of players: ")
 
-for roundNum in range(10):
-    response = requests.get("https://random-word-form.herokuapp.com/random/noun")
-    pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1")
-    print(response.json()[0] + " " + str(pointResponse.json()[0]))
+for playerNum in range(int(numPlayers)):
+    print("Player " + str(playerNum + 1))
+    for roundNum in range(NUM_WORDS):
+        response = requests.get("https://random-word-form.herokuapp.com/random/noun")
+        pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1")
+        print(response.json()[0] + " " + str(pointResponse.json()[0]))
+    
+    time.sleep(120)
 
