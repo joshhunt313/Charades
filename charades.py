0bd04e1004c9e16ab38e6f20f7499b62e42b16a5
Josh Hunt <joshhruns@gmail.com>
2021-11-30 10:26:24 -0500

    Words and points

--- charades.py
+++ charades.py
@@ -1,4 +1,11 @@
 import requests
+import json
+import time
 
-response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
-print(response.json())
+NUM_ROUNDS = 10;
+
+for roundNum in range(10):
+    response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+    pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1")
+    print(response.json()[0] + " " + str(pointResponse.json()[0]))
+    time.sleep(60)
