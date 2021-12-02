6641846870b29ae60315a9011b5c47b9bd72a96c
NguyenJay <jaynguyen02@gmail.com>
2021-12-02 12:04:22 -0500

    Update charades.py

--- charades.py
+++ charades.py
@@ -1,7 +1,7 @@
 import requests
-import json
 import time
 from tkinter import *
+from tkinter import messagebox
 
 NUM_WORDS = 10
 
@@ -9,49 +9,42 @@ window = Tk()
 
 window.title("Charades Games")
 
-window.geometry('300x300')
+window.geometry('300x350')
 
 output_label = Label(window, text="List of Words")
-# num_players_label = Label(window, text="Number of Players")
-# num_players_label.grid(row=0, column=0, sticky=W, pady=2, padx=2)
-
-# num_players_entry = Entry(window)
-# num_players_entry.grid(row=0, column=1, pady=2)
 
 output = Text(window, height = 15, 
               width = 25, 
               bg = "light cyan")
 
+timer = StringVar()
+timer.set("120")
+time_entry = Entry(window, textvariable=timer, width=3)
+
 def run_command():
+    output.delete('1.0', END)
+    temp = int(timer.get())
     charades_text = ""
-    # run_button.configure(text="Running", fg="red")
     for word in range(NUM_WORDS):
         response = requests.get("https://random-word-form.herokuapp.com/random/noun")
         pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=1&max=10&count=1")
         charades_text += response.json()[0] + " " + str(pointResponse.json()[0]) + "\n"
     output.insert(END, charades_text)
 
-run_button = Button(window, text="Run", fg="black", command=run_command,)
-# run_button.grid(column=2, row=1)
+    while temp > -1:
+        timer.set("{0:3d}".format(temp))
+        window.update()
+        time.sleep(1)
+        if temp == 0:
+            messagebox.showinfo("Time Countdown", "Time's up! Calculate your points earned")
+        temp -= 1
 
-# num_players_label.pack()
-# num_players_entry.pack()
+
+run_button = Button(window, text="Run", fg="black", command=run_command)
+
+time_entry.pack()
 output_label.pack()
 output.pack()
 run_button.pack()
 
 window.mainloop()
-
-
-# NUM_WORDS = 10
-
-# numPlayers = input("Numbers of players: ")
-
-# for playerNum in range(int(numPlayers)):
-#     print("Player " + str(playerNum + 1))
-#     for roundNum in range(NUM_WORDS):
-#         response = requests.get("https://random-word-form.herokuapp.com/random/noun")
-#         pointResponse = requests.get("http://www.randomnumberapi.com/api/v1.0/random?min=1&max=10&count=1")
-#         print(response.json()[0] + " " + str(pointResponse.json()[0]))
-#     print("***********************************************")
-#     time.sleep(120)
